b95fd2825bce42f5b11a30cb58e1e7bf36b48c1d
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-03-06 09:30:39 +0200

    Fix working_dir for logs, create_id on first run

--- start.sh
+++ start.sh
@@ -2,6 +2,13 @@
 
 docker-compose up -d tor
 IPADDRESS=$(docker inspect -f "{{ .NetworkSettings.Networks.tarinetwork.IPAddress }}" $(docker-compose ps -q tor))
-cat template_config.toml | sed "s/__REPLACE_ME__/$IPADDRESS/" > tari_config/config.toml
+cat template_config.toml | sed "s/__TOR_CONTAINER_IP__/$IPADDRESS/" > data/tari/config.toml
 docker-compose up wait
-docker-compose run tari
+
+#Check if we have the run --create_id
+if [ ! -f "data/tari/rincewind/node_id.json" ]; then
+  echo "This is the first run, execute create_id"
+  docker-compose run tari tari_base_node --create_id
+else
+  docker-compose run tari
+fi
